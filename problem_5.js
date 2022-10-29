9acbfe9d210a9cc41e89ad3d8b1c2e01594560c5
Nagappan S <Karthiknano932@gmail.com>
2022-10-29 16:00:31 +0530

    Delete problem_5.js

--- problem_5.js
+++
@@ -1,8 +0,0 @@
-function getArrayDepth(value) {
-  return Array.isArray(value) ? 
-    1 + Math.max(0, ...value.map(getArrayDepth)) :
-    0;
-}
-
-let testRy = [1, [2, [3, 4]]];
-console.log(getArrayDepth(testRy));
